6d061d268c53fe6c90cccbb533bb9143b4c02238
umer794 <mu1792697@gmail.com>
2025-02-04 21:27:47 +0500

    Add files via upload

---
+++ JS ASSINGMENT/app.js
@@ -0,0 +1,43 @@
+// 1. Write a script to greet your website visitor using JS alert box.
+//2. Write a script to display following message on your web page
+//3. Write a script to display following message on your web page (Hint : Use line break)
+//4. Write a script to display following messages in sequence
+//5. Generate the following message through browser’s developer console
+//6. Make use of alerts in your new/existing HTML & CSS project
+//7. Practice placement of <script></script> element in following sections of your project in exercise 6:
+//a. Head
+//b. Body (before your page’s HTML)
+//c. Body (inside your page’s HTML)
+//d. Body (after your page’s HTML)
+
+//CHAPTER 2 VARIABLES FOR STRINGS=
+
+//1. Declare a variable called username.
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+alert("Umer")
+alert("20 year old")
+//3. Write script to
+//a) Declare a JS variable, titled message.
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+//Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+alert("i am student web develoment")
+//5. Write a script to display the following alert using one JS variable:
+let pizza = "pizza \n pizz \n pizz \n piz \n pi \n p "
+alert(pizza)
+//6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+//Show the blow mentioned message in an alert box.(Hint: use
+//string concatenation)
+alert("My Email address is exampel@exampel.com ")
+//9. Store following string in a variable and show in alert and browser through JS
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+//Chapter 3 VARIABLES FOR NUMBER
+//1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+alert("i am 20 year old")
+//2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her
+//number of visits on your web page. For example: “You
+//have visited this site N times”.
+alert("you have visited thisis site 14 time")
+//3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
